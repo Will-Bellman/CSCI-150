93504341e2ebc9f41e383855d4207eb318ecaf8d
Will Bellman <bellmanw@xavier.edu>
2022-01-25 21:23:12 +0000

    Created myfirstscript.sh

---
+++ myfirstscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###############################
+# Description: My first script
+# Author: Will Bellman
+# Usage: ./myfirstscript.sh
+###############################
+
+# print the text to the terminal.
+echo "Hello World!"
